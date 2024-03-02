c9044c65932b63f52a8cb64ebbd120d3874ac8e8
shilpi-22 <101976805+shilpi-22@users.noreply.github.com>
2024-03-03 01:51:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ before(() => {
 });
 
 after(() => {
-	console.log( "This part executes once after all tests" );
+	console.log( "This part executes once after all tests " );
 });
 	
 // We can add nested blocks for different tests
