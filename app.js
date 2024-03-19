94c31d51f81d95510e37ca6104287a0ed0e68e94
shilpi-22 <101976805+shilpi-22@users.noreply.github.com>
2024-03-19 13:18:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ app.get('/todo', function (req, res) {
         res.redirect('/todo');
     })
 
+    
     .listen(port, function () {
         // Logging to console
         console.log(`Todolist running on http://0.0.0.0:${port}`)
